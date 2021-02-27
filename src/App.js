192e387509f239ee14507d459f77ebb14d576178
AidenTran15 <aidenkiettran@gmail.com>
2021-02-27 22:38:42 +1100

    Add monster image and create style card

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
-import {CardList} from './components/card-list/card-list';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.jsx";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}));
-
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        console.log(users);
+        this.setState({ monsters: users });
+      });
   }
   render() {
-    return(
-      <div className='App'>
-      <CardList monsters={this.state.monsters}>
-      </CardList>
-    </div>
+    return (
+      <div className="App">
+          <CardList monsters={this.state.monsters} />
+      </div>
     );
   }
 }
